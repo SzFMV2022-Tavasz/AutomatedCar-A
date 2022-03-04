1411f1720f4d0840ef31746bb4805e232b165647
Akos Szabo-Gali <szabogaliakos@gmail.com>
2022-03-04 16:41:40 +0100

    Initial skeleton commit

---
+++ src/AutomatedCar/SystemComponents/VirtualFunctionBus.cs
@@ -0,0 +1,22 @@
+namespace AutomatedCar.SystemComponents
+{
+    using System.Collections.Generic;
+
+    public class VirtualFunctionBus : GameBase
+    {
+        private List<SystemComponent> components = new List<SystemComponent>();
+
+        public void RegisterComponent(SystemComponent component)
+        {
+            this.components.Add(component);
+        }
+
+        protected override void Tick()
+        {
+            foreach (SystemComponent component in this.components)
+            {
+                component.Process();
+            }
+        }
+    }
+}
