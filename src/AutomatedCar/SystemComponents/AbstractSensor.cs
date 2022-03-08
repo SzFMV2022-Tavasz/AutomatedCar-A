19a00de9cbe9ba57f0dfc14f14ea06e5dca89d2a
BasaRichard <32295357+BasaRichard@users.noreply.github.com>
2022-03-08 23:50:43 +0100

    Create AbstractSensor.cs
    Create AbstractSensor class.

---
+++ src/AutomatedCar/SystemComponents/AbstractSensor.cs
@@ -0,0 +1,48 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using System.Collections.Generic;
+    using AutomatedCar.Models;
+    using Avalonia;
+
+    abstract class AbstractSensor
+    {
+        private List<Point> points;
+
+        public int Range { get; set; }
+
+        public double AngleSensor { get; set; }
+
+        public int RightEdge { get; set; }
+
+        public int LeftEdge { get; set; }
+
+        public int Position { get; set; }
+
+        public List<WorldObject> WorldObjects { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AbstractSensor"/> class.
+        /// Default Constructor.
+        /// </summary>
+        /// <param name="range">Sensor range.</param>
+        /// <param name="angleSensor">Sensor angle.</param>
+        /// <param name="position">Sensor position.</param>
+        public AbstractSensor(int range, double angleSensor, int position)
+        {
+            this.Range = range;
+            this.AngleSensor = angleSensor;
+            this.Position = position;
+        }
+
+        /// <summary>
+        /// Calculates the sensor area.
+        /// </summary>
+        /// <returns>Retrun to list of sensor point.</returns>
+        public abstract List<Point> GetSensorArea();
+
+        /// <summary>
+        /// Rotates the angle around the point. (or something similar).
+        /// </summary>
+        public abstract void RotatetoSensor();
+    }
+}
