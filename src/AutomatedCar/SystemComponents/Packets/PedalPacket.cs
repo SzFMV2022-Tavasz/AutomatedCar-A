20d630095ef937849a35a51e9aaeecb393508839
Elekes Zsolt <zsoltelekes09@gmail.com>
2022-03-13 17:59:08 +0100

    Pedal packet created

---
+++ src/AutomatedCar/SystemComponents/Packets/PedalPacket.cs
@@ -0,0 +1,18 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class PedalPacket : ReactiveObject, IPedalPacket
+    {
+        private int gasLevel;
+        private int breakLevel;
+
+        public int GasPedalLevel { get => this.gasLevel; set => this.RaiseAndSetIfChanged(ref this.gasLevel, value); }
+        public int BreakPedalLevel { get =>this.breakLevel; set => this.RaiseAndSetIfChanged(ref this.breakLevel, value); }
+    }
+}
