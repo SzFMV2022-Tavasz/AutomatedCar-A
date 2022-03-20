8ff8e098d653f92e1e7d94a61786e5be805b3543
the_hb <hodak.beno2001@gmail.com>
2022-03-20 11:28:35 +0100

    NPC skeleton added.

---
+++ src/AutomatedCar/Models/NPC/INPC.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomatedCar.Models.NPC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    interface INPC
+    {
+        public void Step(int x, int y, double rotation);
+    }
+}
---
+++ src/AutomatedCar/Models/NPC/NPCCar.cs
@@ -0,0 +1,21 @@
+﻿namespace AutomatedCar.Models.NPC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class NPCCar : Car, INPC
+    {
+        public NPCCar(int x, int y, string filename)
+            : base(x, y, filename)
+        {
+
+        }
+        public void Step(int x, int y, double rotation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
