7bf6ead3f03aecf28b196c5fd53007f0426ba9b7
rolandrevesz <fekete.revesz@gmail.com>
2022-03-24 06:39:10 +0100

    ICollisionCamera interface added

---
+++ src/AutomatedCar/SystemComponents/Sensors/ICollisionCamera.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomatedCar.SystemComponents.Sensors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface ICollisionCamera
+    {
+
+    }
+}
