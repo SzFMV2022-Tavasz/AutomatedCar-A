f3bce87c8f44c17c135d646bc992386654498487
TI-IE_HB <84531896+TI-IEHB@users.noreply.github.com>
2022-03-27 00:36:16 +0100

    Create NPCPed.cs

---
+++ src/AutomatedCar/Models/NPC/NPCPed.cs
@@ -0,0 +1,19 @@
+﻿namespace AutomatedCar.Models.NPC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Helpers;
+
+    class NPCPed : Car, INPC
+    {
+        public NPCPed(int x, int y, string filename)
+            : base(x, y, filename)
+        {
+            this.NPCStatus = new NPCStatus();
+        }
+        public NPCStatus NPCStatus { get; set; }
+    }
+}
