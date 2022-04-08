4007f612339b1f23c12e091edab00abe5ac147fb
attila57 <jancsi.attila57@gmail.com>
2022-04-08 10:21:21 +0200

    Created packet, interface for EmergancyBrake

---
+++ src/AutomatedCar/Helpers/EmergancyBrakeEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace AutomatedCar.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum BrakeStatus
+    {
+        Active,
+        Disabled
+    }
+}
---
+++ src/AutomatedCar/SystemComponents/Packets/EmergencyBrakePacket.cs
@@ -0,0 +1,34 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using System;
+    using AutomatedCar.Models;
+    using ReactiveUI;
+    using AutomatedCar.Helpers;
+
+    class EmergencyBrakePacket : ReactiveObject, IEmergancyBrakePacket
+    {
+
+        private bool activated = false;
+        private BrakeStatus currentstatus;
+        private string currentstate;
+
+        public bool Activated
+        {
+            get => this.activated;
+            set => this.RaiseAndSetIfChanged(ref this.activated, value);
+        }
+
+        public BrakeStatus BrakeStatus
+        {
+            get => this.currentstatus;
+            set => this.RaiseAndSetIfChanged(ref this.currentstatus, value);
+        }
+
+        public string BrakeState
+        {
+            get => this.currentstate;
+
+            set => this.RaiseAndSetIfChanged(ref this.currentstate, value);
+        }
+    }
+}
