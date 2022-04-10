9e1c352ec173ef4df0cb4f91200f3c22a303908a
ISD-ikrk <mate.kreko@gmail.com>
2022-04-10 04:30:34 +0200

    Add interface for controller

---
+++ src/AutomatedCar/SystemComponents/Packets/IControllerPacket.cs
@@ -0,0 +1,19 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IControllerPacket<T>
+    {
+        public T Input { get; }
+        public T Output { get; }
+        public T Target { get; }
+        public T Error { get; }
+        public T LastError { get; }
+
+        public abstract T Transfer(T input);
+    }
+}
