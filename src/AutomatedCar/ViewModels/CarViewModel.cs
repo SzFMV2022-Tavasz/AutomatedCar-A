84343eee74fdf570e646cfb0b7fff40198a43d42
z4k1 <Z4K1@stud.uni-obuda.hu>
2022-04-16 00:17:46 +0200

    Update CarViewModel

--- src/AutomatedCar/ViewModels/CarViewModel.cs
+++ src/AutomatedCar/ViewModels/CarViewModel.cs
@@ -1,18 +1,32 @@
 ﻿namespace AutomatedCar.ViewModels
 {
     using AutomatedCar.Models;
+    using ReactiveUI;
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reactive;
     using System.Text;
     using System.Threading.Tasks;
+    using SystemComponents.Packets;
 
     public class CarViewModel : WorldObjectViewModel
     {
         public AutomatedCar Car { get; set; }
+        public ACCControllerPrototype Acc { get; set; }
+
+        public ReactiveCommand<Unit, Unit> DoTheThing { get; }
         public CarViewModel(AutomatedCar car) : base(car)
         {
             this.Car = car;
+            this.Acc = new ACCControllerPrototype();
+            DoTheThing = ReactiveCommand.Create(DoSomeThing);
+            
+        }
+        
+        void DoSomeThing()
+        {
+            this.Acc.TurnOnOrOff();
         }
     }
 }
