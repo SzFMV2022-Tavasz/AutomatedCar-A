c4ed205aedf494860dfa3a7a436c460de89b2f14
z4k1 <Z4K1@stud.uni-obuda.hu>
2022-04-16 00:14:06 +0200

    ACC dashboard update

--- src/AutomatedCar/Views/DashboardView.xaml.cs
+++ src/AutomatedCar/Views/DashboardView.xaml.cs
@@ -1,7 +1,17 @@
 namespace AutomatedCar.Views
 {
     using Avalonia.Controls;
+    using Avalonia.Controls.Primitives;
+    using Avalonia.Controls.Templates;
+    using Avalonia.Interactivity;
+    using Avalonia.Layout;
+    using Avalonia.LogicalTree;
     using Avalonia.Markup.Xaml;
+    using Avalonia.Media;
+    using Avalonia.VisualTree;
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
 
     public class DashboardView : UserControl
     {
@@ -10,9 +20,32 @@ namespace AutomatedCar.Views
             this.InitializeComponent();
         }
 
+        private bool isOn = false;
+
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void Button_On_Off(object? sender, RoutedEventArgs e)
+        {
+            // todo
+            // hardcoded
+           var myButtonBorder = (Border)this.FindControl<ContentControl>("Dashboard").GetLogicalChildren().ToList()[0].LogicalChildren[11].LogicalChildren[25].LogicalChildren[0].LogicalChildren[0];
+           var labelText = (AccessText)myButtonBorder.GetLogicalChildren().ToList()[0].LogicalChildren[0];
+
+           if (this.isOn)
+           {
+               labelText.DataContext = "Off";
+               myButtonBorder.HorizontalAlignment = HorizontalAlignment.Left;
+           }
+           else
+           {
+               labelText.DataContext = "On";
+               myButtonBorder.HorizontalAlignment = HorizontalAlignment.Right;
+           }
+
+           this.isOn = !this.isOn;
         }
     }
 }
