e5d8752be8bfe3bf0db2d859a0d7ee00688eb06a
z4k1 <Z4K1@stud.uni-obuda.hu>
2022-04-16 00:16:29 +0200

    Add ACC prototype

---
+++ src/AutomatedCar/SystemComponents/Packets/ACCControllerPrototype.cs
@@ -0,0 +1,21 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+
+    public class ACCControllerPrototype:ReactiveObject
+    {
+
+        private bool isOn = false;
+
+        public bool IsOn
+        {
+            get => this.isOn;
+            set => this.RaiseAndSetIfChanged(ref this.isOn, value);
+        }
+
+        public void TurnOnOrOff()
+        {
+            this.IsOn = !this.IsOn;
+        }
+    }
+}
