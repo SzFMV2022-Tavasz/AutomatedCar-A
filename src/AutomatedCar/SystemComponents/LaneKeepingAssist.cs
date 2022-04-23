06be4a7deb298988eadcbe74f8f0db244a007bc0
nemenek <markbodri@gmail.com>
2022-04-23 11:25:45 +0200

    Initialized lane keeping assist base.

---
+++ src/AutomatedCar/SystemComponents/LaneKeepingAssist.cs
@@ -0,0 +1,38 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AutomatedCar.Models;
+
+    public class LaneKeepingAssist : SystemComponent
+    {
+        VirtualFunctionBus bus;
+
+        public LaneKeepingAssist(VirtualFunctionBus bus) : base(bus)
+        {}
+
+        public override void Process()
+        {
+            if (this.bus.SteeringWheelPacket.IsLKAActive)
+            {
+                if (this.bus.SensorPacket.WorldObjectsInRange.Count > 0)
+                {
+                    ICollection<WorldObject> lanes = this.bus.SensorPacket.WorldObjectsInRange.Where(x => x.Filename.Contains("road_2lane")).ToList();
+
+                    if (lanes.Count > 0)
+                    {
+                        this.SetWheelRotationByLanes(lanes);
+                    }
+                }
+            }
+        }
+
+        private void SetWheelRotationByLanes(ICollection<WorldObject> lanes)
+        {
+
+        }
+    }
+}
