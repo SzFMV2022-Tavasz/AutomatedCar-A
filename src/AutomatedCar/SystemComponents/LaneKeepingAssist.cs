a355f0aeec8ef0ece548efef62027a7e284ce8ee
rolandrevesz <fekete.revesz@gmail.com>
2022-05-09 19:16:56 +0200

    creating the DisableLKA method

--- src/AutomatedCar/SystemComponents/LaneKeepingAssist.cs
+++ src/AutomatedCar/SystemComponents/LaneKeepingAssist.cs
@@ -40,5 +40,25 @@
         {
             //Lane keeping calculations
         }
+
+        private void DisableLKA()
+        {
+            ICollection<WorldObject> objects = this.virtualFunctionBus.CameraPacket.WorldObjectsInRange.ToList();
+
+            if (this.virtualFunctionBus.SteeringWheelPacket.IsLKAActive == true)
+            {
+                foreach (var item in objects)
+                {
+                    if (item.Filename == "road_2lane_90left.png" || item.Filename == "road_2lane_90right.png" ||
+                        item.Filename == "road_2lane_crossroad_1.png" || item.Filename == "road_2lane_crossroad_2.png" ||
+                        item.Filename == "road_2lane_rotary.png" || item.Filename == "road_2lane_tjunctionleft.png" ||
+                        item.Filename == "road_2lane_tjunctionright.png")
+                    {
+                        this.virtualFunctionBus.SteeringWheelPacket.IsLKAActive = false;
+                        this.virtualFunctionBus.SteeringWheelPacket.LKAState = "Kezelhetetlen forgalmi szituáció!";
+                    }
+                }
+            }
+        }
     }
 }
